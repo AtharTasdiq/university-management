8dac83b974ed8c0c6c6a9990f160b63f11cc23bd
AtharTasdiq <atharkhantasdiq@gmail.com>
2023-06-06 00:22:34 +0600

    user done

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express, { Application, Request, Response } from 'express'
+import usersRouter from './app/modules/users/users.route'
 import cors from 'cors'
 const app: Application = express()
 
@@ -8,8 +9,12 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+// Application routes
+
+app.use('/api/v1/users/', usersRouter)
+
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!')
+  res.send('Backend Running')
 })
 
 export default app
